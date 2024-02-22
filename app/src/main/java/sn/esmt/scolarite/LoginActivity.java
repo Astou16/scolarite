bc46866e6cc53ee1adbd844f90fad2edf0969e56
hp <assetouconombo49@gmail.com>
2024-02-22 12:38:16 +0000

    Projet Scolarite

--- app/src/main/java/sn/esmt/scolarite/LoginActivity.java
+++ app/src/main/java/sn/esmt/scolarite/LoginActivity.java
@@ -6,19 +6,46 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText emailText;
+    private EditText password;
+    private EditText validerbt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Button connect=(Button) findViewById(R.id.Validerbt);
-        connect.setOnClickListener(new View.OnClickListener() {
+        EditText emailText = (EditText) findViewById(R.id.emailText);
+        EditText password= (EditText) findViewById((R.id.password)) ;
+        Button validerbt= (Button) findViewById(R.id.Validerbt);
+
+
+
+        validerbt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent= new Intent(LoginActivity.this, HomeActivity.class);
-                startActivity(intent);
+                String email = emailText.getText().toString();
+                String pwd = password.getText().toString();
+                if(email.isEmpty() || pwd.isEmpty()){
+                    Toast.makeText(LoginActivity.this, "Veuiller renseigner les champs",Toast.LENGTH_LONG).show();
+                }
+                else
+                    if (email.equals("esmt@esmt.sn") && pwd.equals("123")){
+                        Intent i= new Intent(LoginActivity.this, HomeActivity.class);
+                        startActivity(i);
+                    }
+                    else{
+                        Toast.makeText(LoginActivity.this, "Email ou mot de passe incorrect",Toast.LENGTH_LONG).show();
+
+
+                    }
+
+
             }
         });
     }
